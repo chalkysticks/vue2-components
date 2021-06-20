29341d15e06373ee3b0acda5b6c376f2a500bb23
Matt Kenefick <keneficksays@gmail.com>
2021-06-19 20:58:11 -0400

    Add ViewBase

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,13 @@ import '../node_modules/@chalkysticks/sass/dist/app.css';
 // ---------------------------------------------------------------------------
 
 import { App, Plugin } from 'vue';
+import ViewBase from './Core/Base';
 import BrandingBadge from './Branding/Badge.vue';
 import BrandingStandard from './Branding/Standard.vue';
 import TvVideoTheater from './TV/VideoTheater.vue';
 
 export {
+    ViewBase,
     BrandingBadge,
     BrandingStandard,
     TvVideoTheater,
