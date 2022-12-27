b645f8efe90bc4e2090697a020eac628e62ac8aa
Matt Kenefick <keneficksays@gmail.com>
2022-12-27 17:14:27 -0500

    Update compatibility on the example

---
+++ example/vue.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require('@vue/cli-service')
+
+module.exports = defineConfig({
+    chainWebpack: config => {
+        config.module.rules.delete('eslint');
+    },
+    css: {
+        extract: false
+    }
+});
