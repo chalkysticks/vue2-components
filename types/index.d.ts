47480fdfb45a59e9d29d81c81cda7eb2f2cc9685
Matt Kenefick <keneficksays@gmail.com>
2024-07-16 22:36:31 -0400

    update types

--- types/index.d.ts
+++ types/index.d.ts
@@ -13,3 +13,30 @@ interface IMapPosition {
 	latitude: number;
 	longitude: number;
 }
+
+declare module 'vue2-google-maps' {
+	import { PluginObject } from 'vue';
+
+	export interface LoadOptions {
+		key: string;
+		libraries?: string;
+		v?: string;
+		language?: string;
+		region?: string;
+		[key: string]: any;
+	}
+
+	export interface InstallOptions {
+		load: LoadOptions;
+		installComponents?: boolean;
+	}
+
+	export const install: PluginObject<InstallOptions>;
+	export const GmapMap: any;
+	export const GmapMarker: any;
+	export const GmapInfoWindow: any;
+	export const GmapCircle: any;
+	export const GmapPolygon: any;
+	export const GmapPolyline: any;
+	export const GmapRectangle: any;
+}
