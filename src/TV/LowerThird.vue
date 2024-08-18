14b206c1a05849ff6965407bb99a21dbddd1dd29
Matt Kenefick <keneficksays@gmail.com>
2024-08-18 19:37:51 -0400

    .

--- src/TV/LowerThird.vue
+++ src/TV/LowerThird.vue
@@ -66,32 +66,32 @@
 <style lang="scss">
 	.tv-lowerthird {
 		display: flex;
-		gap: 1rem;
+		gap: 1em;
 		transition: opacity 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94);
 
 		.content {
-			height: 100px;
+			height: 6.25em;
 			position: relative;
 			z-index: 2;
 
 			.title {
 				backdrop-filter: blur(1rem);
 				background-color: rgba(255, 255, 255, 0.85);
-				border-radius: 1rem;
+				border-radius: 1em;
 				border-bottom-left-radius: 0;
 				color: var(--chalky-blue-4);
 				margin-bottom: 2px;
-				min-width: 500px;
-				padding: 1rem;
+				min-width: 30em;
+				padding: 1em;
 				position: relative;
 
 				&:before {
 					background: var(--chalky-red);
-					border-bottom-left-radius: 0.5rem;
-					border-bottom-right-radius: 0.5rem;
+					border-bottom-left-radius: 0.5em;
+					border-bottom-right-radius: 0.5em;
 					bottom: -9px;
 					content: ' ';
-					height: 0.5rem;
+					height: 0.5em;
 					left: 0;
 					opacity: 0.5;
 					position: absolute;
@@ -103,17 +103,46 @@
 			.subtitle {
 				backdrop-filter: blur(1rem);
 				background-color: var(--chalky-red);
-				border-radius: 1rem;
+				border-radius: 1em;
 				border-top-left-radius: 0;
 				border-top-right-radius: 0;
 				color: var(--chalky-white);
 				display: inline-block;
-				padding: 0.5rem 1.25rem;
+				padding: 0.5em 1.25em;
 				position: relative;
 
 				p {
 					margin: 0;
 				}
+			}
+		}
+	}
+
+	// Media Queries
+	// -------------------------------------------------------------------------
+
+	@media only screen and (max-height: 500px) and (orientation: landscape) {
+		.tv-lowerthird {
+			font-size: 14px;
+
+			.chalky.branding-badge {
+				height: 6.25em;
+				width: 6.25em;
+			}
+		}
+	}
+
+	@media only screen and (max-width: 500px) and (orientation: portrait) {
+		.tv-lowerthird {
+			font-size: 10px;
+
+			.content .title::before {
+				bottom: -7px;
+			}
+
+			.chalky.branding-badge {
+				height: 6.25em;
+				width: 6.25em;
 			}
 		}
 	}
