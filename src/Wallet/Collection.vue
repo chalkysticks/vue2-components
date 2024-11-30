7482a08e98e65c8a7e48d1261605eca72adf2169
Matt Kenefick <keneficksays@gmail.com>
2024-11-29 19:59:55 -0500

    Attempt to get wallet working

--- src/Wallet/Collection.vue
+++ src/Wallet/Collection.vue
@@ -2,12 +2,12 @@
 	<section class="chalky wallet-collection">
 		<div class="info">
 			<h5 class="type-small-heading">Wallet</h5>
-			<h4>$2,102</h4>
+			<h4>{{ formattedBalance }}</h4>
 		</div>
 
 		<div class="action">
-			<!-- <button>Collect</button> -->
-			<h5 class="type-small-heading">6 hours until next collection</h5>
+			<button v-if="canCollect" v-on:click="Handle_OnClickCollect">Collect</button>
+			<h5 v-else class="type-small-heading">{{ timeSinceLastCollection }} until next collection</h5>
 		</div>
 	</section>
 </template>
@@ -25,7 +25,64 @@
 	 */
 	@Component
 	export default class WalletCollection extends ViewBase {
-		// Not implemented
+		/**
+		 * @type boolean
+		 */
+		public get canCollect(): boolean {
+			return this.userModel?.lastCollection?.canCollect() || !this.userModel?.lastCollection?.id;
+		}
+
+		/**
+		 * @type string
+		 */
+		public get formattedBalance(): string {
+			const amount: number = this.userModel.getWalletBalance() || 0;
+			const formatter: Intl.NumberFormat = new Intl.NumberFormat('en-US', {
+				currency: 'USD',
+				style: 'currency',
+			});
+			const output = formatter.format(amount);
+
+			return output;
+		}
+
+		/**
+		 * @return string
+		 */
+		protected get timeSinceLastCollection(): string {
+			const walletModel: ChalkySticks.Model.Wallet = this.userModel.lastCollection;
+			const createdAt: Date = new Date(walletModel.getCreatedAt());
+			createdAt.setSeconds(createdAt.getSeconds() + ChalkySticks.Constants.WALLET_COLLECTION_TIME);
+			const timeUntil: string = ChalkySticks.Utility.Format.getRelativeTime(createdAt);
+
+			return timeUntil;
+		}
+
+		/**
+		 * @type ChalkySticks.Model.User
+		 */
+		@Prop({
+			default: () => ChalkySticks.Factory.User.model(),
+		})
+		public userModel!: ChalkySticks.Model.User;
+
+		/**
+		 * @return void
+		 */
+		/**
+		 * @param PointerEvent e
+		 * @return Promise<void>
+		 */
+		protected async Handle_OnClickCollect(e: PointerEvent): Promise<void> {
+			e.preventDefault();
+
+			// Collections
+			const response = await ChalkySticks.Factory.Wallet.model().collect(this.$store.getters['authentication/token']);
+
+			console.log('Response', response);
+
+			this.$forceUpdate();
+		}
 	}
 </script>
 
